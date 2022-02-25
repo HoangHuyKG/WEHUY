6cb91d121df9d02d8c40d426bc20d9a3a3abdcc7
unknown <hoanghuywww10@gmail.com>
2022-02-25 07:40:58 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WL/precache-manifest.c0189895c3fb587a4d2d6760edd522ba.js"
+  "/HoangHuyKG/WL.git/precache-manifest.4e4d2d0949add6cc8c788cf2b4fd4561.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WL/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HoangHuyKG/WL.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
